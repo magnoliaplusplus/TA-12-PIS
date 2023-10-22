324a2dc2db5e1001c87fde4485aebc69a32b7fe2
magnolia <101792488+magnoliaplusplus@users.noreply.github.com>
2023-10-23 00:16:07 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using FitnessCenter.Model;
+using FitnessCenter.Model.Interfaces;
+using FitnessCenter.Output;
+using System;
+using System.Threading;
+
+namespace FitnessCenter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleOutput.Start();
+        }
+    }
+}
